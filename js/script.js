c35b8719868dce7f0a09f18c8df95344519263a0
emiliantimofei <metimo1996@gmail.com>
2024-03-07 17:24:16 +0100

    work in progress

--- js/script.js
+++ js/script.js
@@ -12,3 +12,32 @@
 // Solo una volta che il milestone 1 sarà completo e funzionante allora realizzeremo un form in pagina in cui l’utente potrà inserire i dati e visualizzare il calcolo finale con il prezzo.
 // Il recap dei dati e l'output del prezzo finale, andranno quindi stampati in pagina (il prezzo dovrà essere formattato con massimo due decimali, per indicare i centesimi sul prezzo). Questo richiederà un minimo di ricerca.
 // Buon pomeriggio e buon lavoro !
+
+let elNomeCognome = document.getElementById('nome-cognome');
+
+let elKm = document.getElementById(parseInt('km'));
+let priceOfTrip = (0.21 * elKm);
+let priceMinor = (priceOfTrip - (priceOfTrip * 0.2));
+let priceOld = (priceOfTrip - (priceOfTrip * 0.4));
+
+let elFascia = document.getElementById('fascia');
+ 
+let elGenera = document.getElementById('genera');
+let elAnnulla = document.getElementById('annulla');
+
+// console.log(elNomeCognome, elKm, elFascia, elGenera, elAnnulla);
+let elBox1 = document.querySelector('.box-1');
+let elBox2 = document.querySelector('.box-2');
+let elBox3 = document.querySelector('.box-3');
+let elBox4 = document.querySelector('.box-4');
+let elBox5 = document.querySelector('.box-5');
+
+elGenera.addEventListener('click', function(){
+    let testo = elNomeCognome.value;
+    let number = elKm.value;
+    let age = elFascia.value;
+    console.log(testo, number, age);
+    let elBox5 = 
+    elBox1.innerHTML = testo;
+    elBox2.innerHTML = age;
+});
